22ddce27372c08200f7e52b724887a6245df45c0
Илья Кузнецов <jumpdigital.studio@gmail.com>
2022-12-07 18:53:42 +0500

    Обновление папок

---
+++ Малышев А.О (C#)/Демо-экзамен/CommerceProject/CreationStore/Run.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CreationStore
+{
+    internal static class Run
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Authorization());
+        }
+
+        static void DataBseConnect()
+        {
+
+        }
+    }
+}
